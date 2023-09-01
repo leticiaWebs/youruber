3ddde7f5fa61ba850cf8a8c2243954ff8b4774cc
leticiaWebs <leticiaaod@gmail.com>
2023-09-01 16:59:49 -0300

    Youruber project is create

---
+++ backend/src/main/java/com/youruber/youruber/YouruberApplication.java
@@ -0,0 +1,13 @@
+package com.youruber.youruber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YouruberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YouruberApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/youruber/youruber/YouruberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.youruber.youruber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YouruberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
