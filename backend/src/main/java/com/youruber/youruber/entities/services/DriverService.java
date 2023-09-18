a5b1413c91969d7aca08417ce7b3f4adcad78caa
leticiaWebs <leticiaaod@gmail.com>
2023-09-18 17:44:23 -0300

    Funções de update e delete para objeto driver

--- backend/src/main/java/com/youruber/youruber/entities/services/DriverService.java
+++ backend/src/main/java/com/youruber/youruber/entities/services/DriverService.java
@@ -7,12 +7,15 @@ import java.util.stream.Collectors;
 import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.youruber.youruber.dto.DriverDTO;
 import com.youruber.youruber.entities.Driver;
 import com.youruber.youruber.entities.services.exceptions.ResourceNotFoundException;
+import com.youruber.youruber.resources.exceptions.DataBaseException;
 import com.youruber.youruber.respositories.DriverRepository;
 
 @Service
@@ -64,6 +67,19 @@ public class DriverService {
      }
      
 	}
-  
 
+	public void delete(Integer id) {
+	 try {
+		repository.deleteById(id);
+	}
+	catch(EmptyResultDataAccessException e) {
+		throw new ResourceNotFoundException("Id not found" +id);
+	}
+	 
+	catch (DataIntegrityViolationException e ) {
+		throw new DataBaseException("Integrity violation");
+		
+	}
+  
+	}
 }
---
+++ backend/src/main/java/com/youruber/youruber/resources/exceptions/DataBaseException.java
@@ -0,0 +1,12 @@
+package com.youruber.youruber.resources.exceptions;
+
+public class DataBaseException extends RuntimeException {
+	private static final long serialVersionUID = 4285766557842589336L;
+	
+	public DataBaseException(String msg) {
+		super(msg);
+	}
+	
+	
+
+}
